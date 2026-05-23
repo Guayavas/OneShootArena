49ff1b2976f16a7120cd25305333e8a5814da755
Guayavas <victortdh1@gmail.com>
2026-05-23 11:52:53 -0500

    Subir cambios

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,8 @@
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    
 }
