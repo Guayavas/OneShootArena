b32561e4017dec0736020f59dc93a71bd9dff695
Guayavas <victortdh1@gmail.com>
2026-05-31 01:08:50 -0500

    Version lista para probar su despliegue en git hub pages

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -25,14 +25,9 @@ self.addEventListener('fetch', function (e) {
       if (response) { return response; }
 
       response = await fetch(e.request);
-
-      // Only cache GET requests
-      if (e.request.method === 'GET') {
-          const cache = await caches.open(cacheName);
-          console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-          cache.put(e.request, response.clone());
-      }
-
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
       return response;
     })());
 });
